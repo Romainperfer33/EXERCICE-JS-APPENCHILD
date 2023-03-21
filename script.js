0344ef20fc1666da818cdc2ec0fcdc4b342b202b
Romain Pereira Ferreira <romain.pereira@lapiscine.pro>
2023-03-21 14:53:14 +0100

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-// Je recupere le bouton dans mon HTML est en crée une constante afin de pouvoir la reutiliser dans ma fonction.
+
 const btn = document.querySelector('button')
 
-// J'ajoute un element d'ecoute a ma constante "btn" pour lui signifier que a chaque click, une action devra se realiser
-// cette acttion est placée dans une fonction dite anonyme car elle est stipule que pour un seul BTN
-btn.addEventListener('click',() => {
-// J'indique la création d'un titre à inserer dans mon HTML et stocker cette information dans une constante
- const title = document.createElement("h1");
-//  Je stipule que la contstante title devra incorperer du texte "Mes recettes"
- title.textContent = "Mes recettes";
-//  Afin d'inserer les elements precedant je dois recuperer la div ou article dans lequel il se situe
-// ici on recupere l'emplacement de l'id recipe
-// Une fois l'infomration d'emplacement recuperé on la stock dans un variable
- const recipeContainer = document.getElementById("recipe");
-//  JS est assez nourrit d'inforamtion, c'est pour cela qu'on lui demande d'ajouter à recipeContainer en enfant la constante title
- recipeContainer.appendChild(title);
+const titleh1 = [{
+    title: "Ma recette 1",
+    image: "https://picsum.photos/200/300",
+    content: "Bonjour la piscine blablazblzlbla 1"
+  }, {
+    title: "Ma recette 2",
+    image: "https://picsum.photos/300/300",
+    content: "Bonjour la piscine blablazblzlbla 2"
+  }, {
+    title: "Ma recette 3",
+    image: "https://picsum.photos/400/300",
+    content: "Bonjour la piscine blablazblzlbla 3"
+  }];
 
+
+btn.addEventListener('click',() => {
+
+const globalChange = document.getElementById("recipe")
+globalChange.innerHTML = "";
+
+const randomIndex = Math.floor(Math.random()* titleh1.length)
+
+const randomTitle = document.createElement("h1");
+randomTitle.textContent = titleh1[randomIndex].title;
+
+
+ const para = document.createElement('p');
+ para.textContent = titleh1[randomIndex].content;
+
+const imgAm = document.createElement('img')
+imgAm.src = "./img/AMEL.jpeg"
+
+recipe.append(randomTitle,para,imgAm);
 })
+
